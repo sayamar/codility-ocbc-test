85ddf1fc63861d4340e8e8574712ed70e0a72900
Maruthi SAYAMPU <maruthi.sayampu@gmail.com>
2023-08-26 02:41:29 -0700

    initial commit

---
+++ src/main/java/com/solution/articles/codilityocbctest/CodilityOcbcTestApplication.java
@@ -0,0 +1,13 @@
+package com.solution.articles.codilityocbctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodilityOcbcTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodilityOcbcTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solution/articles/codilityocbctest/CodilityOcbcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solution.articles.codilityocbctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodilityOcbcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
